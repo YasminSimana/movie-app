b2bcb5822029759e39a65254165d36b9a489ab04
yasmin <yasminsheffer21@gmail.com>
2020-12-30 00:45:57 +0200

    filter works and added select box with error

--- src/pages/ActorsGallery.js
+++ src/pages/ActorsGallery.js
@@ -4,6 +4,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
 import { useState } from "react";
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,13 +18,30 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: "white",
       borderRadius: 14,
     },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        backgroundColor: "white",
+        borderRadius: 14,
+        display: "inline",
+      },
+      selectEmpty: {
+        marginTop: theme.spacing(2),
+        backgroundColor: "white",
+        borderRadius: 14,
+      },
+      lable: {  
+        backgroundColor: "white",
+        borderRadius: 14,
+        display: "inline",
+      },
   },
 }));
 
 function ActorsGallery(props) {
     const {actorsArr} = props;
-    
     const classes = useStyles();
+
     let actorsData = [];
     let cardsTempData = [];
     for (let i of actorsArr) {
@@ -29,6 +51,11 @@ function ActorsGallery(props) {
     }
 
     const [cardsArr, setCardsArr] = useState(cardsTempData);
+    const [age, setAge] = useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+      };
 
     function filterActors(e){
         setCardsArr([]);
@@ -41,11 +68,41 @@ function ActorsGallery(props) {
         setCardsArr(cardsTempData);
     }
 
+    function sortActors(e) {
+        setCardsArr([]);
+        cardsTempData = [];
+        const sortSubject = e.target.value;
+        console.log(sortSubject)
+        if (sortSubject === "First name"){
+            actorsData.sort((a, b) => (a.firstName > b.firstName) ? 1 : -1)
+        }
+        else if (sortSubject === "Last name"){
+            actorsData.sort((a, b) => (a.lastName > b.lastName) ? 1 : -1)
+        }
+        else if (sortSubject === "Age"){
+            actorsData.sort((a, b) => (a.getAge() > b.getAge()) ? 1 : -1)
+        }
+        setCardsArr(actorsData);
+    }
+
     return(
         <div>            
             <form className={classes.root} noValidate autoComplete="off">
                 <TextField id="standard-basic" label="Search.." onChange={filterActors}/>
             </form>
+            <FormControl className={classes.formControl}>
+                <InputLabel id="demo-simple-select-label" onChange={sortActors}>Sort</InputLabel>
+                <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                onChange={handleChange}
+                >
+                <MenuItem value={10}>First name</MenuItem>
+                <MenuItem value={20}>Last name</MenuItem>
+                <MenuItem value={30}>Age</MenuItem>
+                </Select>
+            </FormControl>
             <Box display="flex" justifyContent="center" flexWrap="wrap">
                 {cardsArr}
             </Box>
